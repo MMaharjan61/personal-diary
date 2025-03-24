fd0ace6285d8e28cdf9bdad080c6739cd20f473f
LukasF7889 <lukas_fritsch@hotmail.de>
2025-03-24 13:45:40 +0100

    Added general styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div className="text-2xl">Hello</div>;
+  return (
+    <div>
+      <h1>This is a H1</h1>
+      <h2>This is an H2</h2>
+      <p>
+        This is an paragraph with an <a>link in it</a>{" "}
+      </p>
+      <button>This is an button</button>
+    </div>
+  );
 };
 
 export default App;
