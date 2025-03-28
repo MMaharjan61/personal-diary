465b7903da0a10e2e6a2a3389b0430a5840aa320
Manoj Maharjan <manoj.maharjan@hotmail.com>
2025-03-28 18:45:19 +0100

    Added button to upload image

--- src/componentsNew/AddEntry.jsx
+++ src/componentsNew/AddEntry.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-import { loadEntries } from "./Storage";
 
 const AddEntry = ({ onAddEntry, onCancel }) => {
-  const today = new Date().toISOString().split("T")[0];
-  const existingEntries = loadEntries();
-  //const hasEntryToday = existingEntries.some((entry) => entry.date === today);
+  const today = new Date().toLocaleDateString("sv-SE"); // Format to yyyy-mm-dd
 
   const [title, setTitle] = useState("");
   const [date, setDate] = useState(today);
@@ -12,18 +9,19 @@ const AddEntry = ({ onAddEntry, onCancel }) => {
   const [content, setContent] = useState("");
   const [error, setError] = useState("");
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file); // Create a URL for the local image
+      setImage(imageUrl); // Store the image URL in your state
+    }
+  };
+
   const handleSubmit = () => {
     if (!title || !date || !image || !content) {
       setError("Please fill in all fields.");
       return;
     }
-
-    /*if (hasEntryToday) {
-      setError(
-        "You have already added an entry for today. Please come back tomorrow!"
-      );
-      return;
-    }*/
 
     const newEntry = { title, date, image, content };
     onAddEntry(newEntry);
@@ -34,40 +32,57 @@ const AddEntry = ({ onAddEntry, onCancel }) => {
       <div className="bg-[#082140] p-6 rounded-lg w-96 relative">
         <h2 className="text-2xl font-bold mb-4">New entry</h2>
         {error && <p className="text-red-500 mb-4">{error}</p>}
+        <p className="text-white">Title</p>
         <input
           type="text"
           className="w-full p-2 mb-4 bg-white rounded"
-          placeholder="Title"
+          placeholder="Enter a title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        <p className="text-white">Date</p>
         <input
           type="date"
           className="w-full p-2 mb-4 bg-white rounded"
           value={date}
           onChange={(e) => setDate(e.target.value)}
         />
+        <p className="text-white">Image</p>
         <input
           type="text"
           className="w-full p-2 mb-4 bg-white rounded"
-          placeholder="Image URL"
+          placeholder='Upload an image via "Choose file"'
           value={image}
           onChange={(e) => setImage(e.target.value)}
         />
+        <label
+          htmlFor="file-upload"
+          className="flex items-center cursor-pointer mb-4 p-2 bg-[#c18234] text-white text-sm font-semibold rounded w-fit hover:bg-[#F0A240]"
+        >
+          Choose file
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+        <p className="text-white">Content</p>
         <textarea
-          className="w-full p-2 mb-4 bg-white rounded"
-          placeholder="Content"
+          className="w-full p-2 mb-6 bg-white rounded"
+          placeholder="Write your text here"
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
         <button
-          className="px-2 py-1 border-[#F0A240] text-[#F0A240] text-sm rounded hover:bg-[#c18234] hover:text-[#FFFFFF]"
+          className="mb-4 p-2 bg-[#c18234] text-white text-sm rounded outline-none hover:bg-[#F0A240]"
           onClick={handleSubmit}
         >
           Save entry
         </button>
         <button
-          className="absolute bottom-6 right-6 px-2 py-1 border-[#F0A240] text-[#F0A240] text-sm rounded hover:bg-[#c18234] hover:text-[#FFFFFF]"
+          className="mb-4 p-2 right-6 absolute bg-[#c18234] text-white text-sm rounded outline-none hover:bg-[#F0A240]"
           onClick={onCancel}
         >
           Cancel
