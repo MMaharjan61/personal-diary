689e38e0138e2a5b5242d44bf2109eaa08d80eb3
DS85300 <sagan@parkstern.de>
2025-03-28 13:40:20 +0100

    breakingPointsaddedfor Calendar

--- src/componentsNew/Calendar.jsx
+++ src/componentsNew/Calendar.jsx
@@ -2,10 +2,7 @@ import Calendar from 'react-calendar'
 
 
     const DiaryCalendar = ({ entries,setFilter }) => {
-        const [filter, setFilter] = useState(null);
-    const handleDateChange = (date) => {
-        const formattedDate = date.toISOString().split("T")[0];
-        setFilter(formattedDate);
+ 
     }
 
 
